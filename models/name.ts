95da6a139f81c6f39ed2beae86d112e418e6e596
saugat1070 <saugatgiri1070@gmail.com>
2025-12-07 21:38:03 +0545

    index and name of attributes of appwrite

---
+++ models/name.ts
@@ -0,0 +1,7 @@
+export const db = "main-stackflow"
+export const questionCollectioin = "question";
+export const answerCollection = "answers"
+export const commentCollection = "comments"
+export const voteCollection = "votes"
+
+export const questionAttachmentBucket = "question-attchment";
